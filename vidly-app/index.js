acc2b2a8d8b341d249a39b5e5c871ed792673af7
Mark Anthony Gutierrez <14417@olinkgroup.com>
2018-07-27 09:52:41 +0800

    Added Section: 10 - Authentication and Authorization

--- vidly-app/index.js
+++ vidly-app/index.js
@@ -1,23 +1,33 @@
+const config = require('config');
 const Joi = require('joi');
 Joi.objectId = require('joi-objectid')(Joi);
 const mongoose = require('mongoose');
 const express = require('express');
 const app = express();
 
-mongoose.connect('mongodb://admin:password123@ds147411.mlab.com:47411/db_vidly', { useNewUrlParser: true })
-    .then(() => console.log('Connected to MongoDB..'))
-    .catch(err => console.error('Cannot connect to database..', err));
-
 const genres = require('./routes/genres');
 const customers = require('./routes/customers');
 const movies = require('./routes/movies');
 const rentals = require('./routes/rentals');
+const users = require('./routes/users');
+const auth = require('./routes/auth');
+
+if(!config.get('jwtPrivateKey')) {
+    console.error('FATAL ERROR: jwtPrivateKey is not define');
+    process.exit(1);
+}
+
+mongoose.connect('mongodb://admin:password123@ds147411.mlab.com:47411/db_vidly', { useNewUrlParser: true })
+    .then(() => console.log('Connected to MongoDB..'))
+    .catch(err => console.error('Cannot connect to database..', err));
 
 app.use(express.json());
 app.use('/api/genres', genres);
 app.use('/api/customers', customers);
 app.use('/api/movies', movies);
 app.use('/api/rentals', rentals);
+app.use('/api/users', users);
+app.use('/api/auth', auth);
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening to port ${ port }...`));
--- x.js
+++ x.js
@@ -29,3 +29,11 @@ let course = {
         name: 'Mac'
     }
 }
+
+// Section 10 - Authentication and Authorization
+
+// Authentication -is the process of identifying if the user is who they claim they are, for example logging in
+// Authorization - is determining if the user has the right permission to perform the given operation
+
+// Register User: POST /api/users {name, email {unique: true}, password}
+// Login User: POST /api/logins
