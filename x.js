bf60da5ff1d8913e42200450229450109ff162ab
Mark Anthony Gutierrez <14417@olinkgroup.com>
2018-08-02 09:55:53 +0800

    Added Section: 14 - Test-driven Development

--- x.js
+++ x.js
@@ -37,3 +37,19 @@ let course = {
 
 // Register User: POST /api/users {name, email {unique: true}, password}
 // Login User: POST /api/logins
+
+
+// Section 14: TDD
+
+// POST /api/returns {customerId, movieId}
+
+// Return 401 if client is not logged in
+// Return 400 if customerId is not provided
+// Return 400 if movieId is not provided
+// Return 404 if no rental found for this customer/movie
+// Return 400 if rental already processed
+// Return 200 if valid request
+// Set the return date
+// Calculate the rental fee
+// Increase the stock
+// Return the rental
